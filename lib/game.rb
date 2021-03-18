773d0b4e5897aaedc1f390dce1267b513cc170ce
Julia Kim <juliaykim6@gmail.com>
2021-03-18 18:35:49 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -22,6 +22,13 @@ class Game
       @board.turn_count % 2 == 0 ? @player_1 : player_2
     end
 
+    def won?
+      WIN_COMBINATIONS.detect do |combo|
+         @board.cells[combo[0]] == @board.cells[combo[1]] &&
+         @board.cells[combo[1]] == @board.cells[combo[2]] &&
+         @board.taken?(combo[0]+1)
+       end
+   end
 
-    
+
   end
