a7ee35dca903b9e96ac9b25c65593a247dec075a
Julia Kim <juliaykim6@gmail.com>
2021-03-18 18:35:07 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -6,4 +6,19 @@ class Game
     @player_2 = player_2
     @board = board
   end
-end
+
+  WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,4,2]
+    ]
+
+    def current_player
+      @board.turn_count % 2 == 0 ? @player_1 : player_2
+
+endend
