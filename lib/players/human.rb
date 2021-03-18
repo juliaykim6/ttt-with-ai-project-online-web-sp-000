f71682a62ce6e29bc63827245b22bd60ec7cc971
Julia Kim <juliaykim6@gmail.com>
2021-03-18 18:30:55 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,9 +1,10 @@
 module Players
-  class Human < Player
+    class Human < Player
 
-    def move(board)
-      puts "Please enter 1-9:"
-      gets.strip
+        def move(board)
+            puts "Please enter 1-9:"
+            gets.strip
+
+        end
     end
-  end
-end
+end 
